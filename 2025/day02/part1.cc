13c160d9a71cabfe04eb7d67b54fe7bea5ad057c
Mairex1337 <m.lindenburger.1@student.rug.nl>
2025-12-04 21:22:43 +0100

    day2

--- 2025/day02/part1.cc
+++ 2025/day02/part1.cc
@@ -4,13 +4,9 @@
 
 size_t is_invalid(string const &curr)
 {
-    size_t first = 0, second = curr.length() / 2;
-    for (size_t stop = second; first != stop; ++first, ++second)
-    {
-        if (curr[first] != curr[second])
-            return 0;
-    }
-    return stoul(curr);
+    size_t half = curr.length() / 2;
+    return equal(curr.begin(), curr.begin() + half, curr.begin() + half) ?
+        stoul(curr) : 0;
 }
 
 size_t find_invalid(size_t start, size_t end)
@@ -41,7 +37,6 @@ int main()
     size_t result = 0, idx = 0;
     while (getline(ifs, str, ','))
     {
-        cout << str << "\n";
         idx = str.find('-');
         result += find_invalid(stoul(str.substr(0, idx)), stoul(str.substr(idx + 1)));
     }
